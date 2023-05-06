fix metrics computation

utils.py
@@ -17,7 +17,7 @@ def accuracy(pred_y, y):
 
 def compute_metrics(model, name, data, df):
 
-  _, y_predicted = model((data.x, data.edge_index)).to("cpu").max(dim=1)
+  _, y_predicted = model((data.x, data.edge_index))[0].to("cpu").max(dim=1)
   data = data.to("cpu")
 
   prec_ill,rec_ill,f1_ill,_ = precision_recall_fscore_support(data.y[data.test_mask], y_predicted[data.test_mask], average='binary', pos_label=0)
@@ -25,6 +25,8 @@ def compute_metrics(model, name, data, df):
 
   df = df.append({'model': name, 'Precision': np.round(prec_ill,3), 'Recall': np.round(rec_ill,3), 'F1': np.round(f1_ill,3),
    'F1 Micro AVG':np.round(f1_micro,3)}, ignore_index=True)
+   
+  return df
 
 def plot_results(df):
 