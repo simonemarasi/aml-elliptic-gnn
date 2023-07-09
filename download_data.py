62b522dfcda10a80ee8fb0a1145111e509f4bb0c
simonemarasi <simone.marasi@studio.unibo.it>
2023-07-09 21:43:10 +0200

    update data url

--- download_data.py
+++ download_data.py
@@ -9,7 +9,8 @@ parser.add_argument("-p", "--path", dest="path", help="Path where save folder", 
 command_line_args = parser.parse_args()
 data_path = command_line_args.path
 
-URL = 'https://archive.org/download/elliptic-dataset/d4w7q6.zip'
+# URL = 'https://archive.org/download/elliptic-dataset/d4w7q6.zip'
+URL = 'https://www.4sync.com/web/directDownload/fQErng3L/5YfHxh7W.cc4f36f14c07d75ced4bf1fcfa1a0772'
 
 r = requests.get(URL)
 os.makedirs(os.path.join(data_path, 'tmp'), exist_ok=True)
